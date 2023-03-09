1ad2e5a2a393b4c1c36b91c6c9117bbafb24d0a0
YauheniKastsevich <evgecha123@gmail.com>
2023-03-09 20:51:49 +0300

    4

---
+++ sandbox/js/freecodecamp/BasicJavaScript/task18.js
@@ -0,0 +1,27 @@
+////// цикл while
+
+// Setup
+const myArray = [];
+let i = 5;
+
+while (i > -1){
+  myArray.push(i);
+  i--;
+}
+console.log(myArray)
+// Only change code below this line
+
+
+////// цикл for
+
+// Setup
+const myArray1 = [];
+
+for(let i=1; i<6; i++){
+  myArray1.push(i);
+}
+console.log(myArray1)
+// Only change code below this line
+
+///////
+

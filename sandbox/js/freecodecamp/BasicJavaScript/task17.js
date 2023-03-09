4aa5015eefcb82fe5b9b31c09f5b460868f2bab1
YauheniKastsevich <evgecha123@gmail.com>
2023-03-09 20:46:09 +0300

    3

--- sandbox/js/freecodecamp/BasicJavaScript/task17.js
+++ sandbox/js/freecodecamp/BasicJavaScript/task17.js
@@ -108,7 +108,130 @@ function phoneticLookup(val) {
 
 phoneticLookup("charlie");
 
+/////// Проверка объекта на свойства
+
+function checkObj(obj, checkProp) {
+  // Only change code below this line
+  if (obj.hasOwnProperty(checkProp)){
+  return obj[checkProp]}
+  else{
+    return "Not Found"
+  }
+ 
+  // Only change code above this line
+}
+
+////// Несколько массивов в одном объекте
+
+const myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true},
+    {"artist": "Bill Joel",
+    "title": "Pian Man",
+    "release_year": 197,
+    "formats": [
+      "D",
+      "T",
+      "P"
+    ]
+  }
+];
+
+////// Доступ к вложенным объектам
+
+const myStorage = {
+  "car": {
+    "inside": {
+      "glove box": "maps",
+      "passenger seat": "crumbs"
+     },
+    "outside": {
+      "trunk": "jack"
+    }
+  }
+};
+
+const gloveBoxContents = myStorage.car.inside["glove box"];
+
+  //////Доступ к вложенным массивам
+
+  const myPlants = [
+    {
+      type: "flowers",
+      list: [
+        "rose",
+        "tulip",
+        "dandelion"
+      ]
+    },
+    {
+      type: "trees",
+      list: [
+        "fir",
+        "pine",
+        "birch"
+      ]
+    }
+  ];
+  
+  const secondTree = myPlants[1].list[1];
+
+  ///// Дополнить КОЛЛЕКЦИЮ ЗАПИСЕЙ
+/*
+
+
+
+
+  // Setup
+const recordCollection = {
+  2548: {
+    albumTitle: 'Slippery When Wet',
+    artist: 'Bon Jovi',
+    tracks: ['Let It Rock', 'You Give Love a Bad Name']
+  },
+  2468: {
+    albumTitle: '1999',
+    artist: 'Prince',
+    tracks: ['1999', 'Little Red Corvette']
+  },
+  1245: {
+    artist: 'Robert Palmer',
+    tracks: []
+  },
+  5439: {
+    albumTitle: 'ABBA Gold'
+  }
+};
+
+// Only change code below this line
+function updateRecords(records, id, prop, value) {
+  if(prop !== "tracks" && value !== ""){
+    records[id][prop] = value;
+  }else if(prop === "tracks" && records[id].hasOwnProperty("tracks") === false){
+    records[id][prop] = [value];
+  }else if(prop === "tracks" && value !== ""){
+    records[id][prop].push(value);
+  }else if(value === ""){
+    delete records[id][prop];
+  }
+return records;
+}
+
+updateRecords(recordCollection, 5439, 'artist', 'ABBA');
+ 
+
+
+*/
 ///////
+
 
 
 

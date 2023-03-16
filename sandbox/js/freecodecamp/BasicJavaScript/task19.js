5677d91e50eca1812d75f600466411cde5dc837d
YauheniKastsevich <evgecha123@gmail.com>
2023-03-16 18:34:34 +0300

    a9

--- sandbox/js/freecodecamp/BasicJavaScript/task19.js
+++ sandbox/js/freecodecamp/BasicJavaScript/task19.js
@@ -86,9 +86,3 @@ function rangeOfNumbers(startNum, endNum) {
   return startNum <= endNum ? [startNum].concat(rangeOfNumbers(startNum+1, endNum)) : []
 }
 console.log(rangeOfNumbers(5, 10));
-
-
-//////
-
-
-
